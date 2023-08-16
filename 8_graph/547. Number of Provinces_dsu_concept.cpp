860d35d1e73a395c7e76ce529fa3e455ac2d9bca
satyajit patel <satyajitpatel770001@gmail.com>
2023-08-17 01:44:05 +0530

    update

--- 8_graph/547. Number of Provinces_dsu_concept.cpp
+++ 8_graph/547. Number of Provinces_dsu_concept.cpp
@@ -1,12 +1,12 @@
-// https://leetcode.com/problems/number-of-provinces/description/
 class DSU {
-private:
-    std::vector<int> parent, size;
 public:
-    DSU(int N) {
-        parent.resize(N);
-        for(int i=0; i<N; i++) parent[i] = i;
-        size.resize(N, 1);
+    std::vector<int> parent, size;
+
+    DSU(int n) {
+        for(int i=0; i<=n; i++) {
+            parent.push_back(i);
+            size.push_back(1);
+        }
     }
 
     int getParent(int node) {
@@ -19,9 +19,6 @@ public:
         int nv = getParent(v);
         if(nu == nv) return;
         if(size[nu] < size[nv]) {
-	// smaller going to attached to bigger
-	// so nu got attached to nv
-	// so size of nv got increased by size of nu
             parent[nu] = nv;
             size[nv] += size[nu];
         }
@@ -31,22 +28,26 @@ public:
         }
     }
 };
+
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        DSU* dsu = new DSU(isConnected.size()+1);
-        for(int i=0; i<isConnected.size(); i++) {
-            for(int j=0; j<isConnected[i].size(); j++) {
-                if(isConnected[i][j] == true) {
-                    int u = i+1;
-                    int v = j+1;
+        int n = isConnected.size();
+        int m = isConnected[0].size();
+        DSU* dsu = new DSU(n+5);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                int flag = isConnected[i][j];
+                if(flag) {
+                    int u = i + 1;
+                    int v = j + 1;
                     if(dsu->getParent(u) != dsu->getParent(v)) dsu->unionS(u, v);
                 }
             }
         }
         int cc = 0;
-        for(int root=1; root<=isConnected.size(); root++) {
-            if(dsu->getParent(root) == root) cc++;
+        for(int node=1; node<=n; node++) {
+            if(dsu->getParent(node) == node) cc += 1;
         }
         return cc;
     }
